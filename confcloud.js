c437864b7917622039521cf76b8cb9aa44ba3d3b
esouthers <153128812+esouthers@users.noreply.github.com>
2023-12-15 09:29:38 -0600

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -492,9 +492,9 @@ function confCloudJS() {
                   var tabPageTitle = data.parameters.title.value;
                   var tabPageID = $(this)[0].url.split('macro/id/')[1].split('/')[0];
                   $('#aui-tabs' + tabsIndex + ' .tabs-menu').append('<li class="menu-item"><a href="#' + tabPageTitle.replace(' ','') + '">' + tabPageTitle + '</a></li>');
-                  $('#aui-tabs' + tabsIndex + '.menu-item').first().addClass('active-tab');
+                  $('#aui-tabs' + tabsIndex + ' .menu-item').first().addClass('active-tab');
                   $('#aui-tabs' + tabsIndex).append('<div id="' + tabPageTitle.replace(' ','') + '" data-pane-title="' + tabPageTitle + '" class="cfm tabs-pane" role="tabpanel" loaded="true" style="display: none;"></div>');
-                  $('#aui-tabs' + tabsIndex + '.tabs-pane').first().addClass('active-pane').show();
+                  $('#aui-tabs' + tabsIndex + ' .tabs-pane').first().addClass('active-pane').show();
                   $.get("https://corsproxy.io/?https://dochaivision.atlassian.net/wiki/rest/api/content/" + pageID + "/history/0/macro/id/" + tabPageID + "/convert/view", function( data ) {
 //                    console.log(data.value);
                     $('#' + tabPageTitle.replace(' ','')).append(data.value);

5653580f2c95bcf61da606c9e57d95fb6b90a8ce
esouthers <153128812+esouthers@users.noreply.github.com>
2024-01-08 17:50:40 -0600

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -648,7 +648,7 @@ function confCloudJS() {
             }
           }
 
-          function getSearchIndexes() {
+          function getSearchIndexes(numResults) {
             const params = new Proxy(new URLSearchParams(window.location.search), {
               get: (searchParams, prop) => searchParams.get(prop),
             });

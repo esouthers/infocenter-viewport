a4841090704f8e8be4b87dda5e919b1dcf3477eb
esouthers <153128812+esouthers@users.noreply.github.com>
2024-01-26 23:31:19 -0600

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -736,11 +736,9 @@ function confCloudJS() {
     }
     $.fn.changeElementType = function(newType) {
         var attrs = {};
-
         $.each(this[0].attributes, function(idx, attr) {
             attrs[attr.nodeName] = attr.nodeValue;
         });
-
         this.replaceWith(function() {
             return $("<" + newType + "/>", attrs).append($(this).contents());
         });

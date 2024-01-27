690683c486dc9742c19a9cd9d40bbd4a5d9f977b
esouthers <153128812+esouthers@users.noreply.github.com>
2024-01-26 23:30:57 -0600

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -734,19 +734,17 @@ function confCloudJS() {
         }          
       }
     }
-    (function($) {
-        $.fn.changeElementType = function(newType) {
-            var attrs = {};
+    $.fn.changeElementType = function(newType) {
+        var attrs = {};
 
-            $.each(this[0].attributes, function(idx, attr) {
-                attrs[attr.nodeName] = attr.nodeValue;
-            });
+        $.each(this[0].attributes, function(idx, attr) {
+            attrs[attr.nodeName] = attr.nodeValue;
+        });
 
-            this.replaceWith(function() {
-                return $("<" + newType + "/>", attrs).append($(this).contents());
-            });
-        };
-    })(jQuery);
+        this.replaceWith(function() {
+            return $("<" + newType + "/>", attrs).append($(this).contents());
+        });
+    };
     function convertExpandsToTabs() {
       $('.expand-container').each(function() {
         let thisNext = $(this).next();

2e73812adffd5d86d9ac193d6dbca46fe65c624d
esouthers <153128812+esouthers@users.noreply.github.com>
2024-01-28 01:49:43 -0600

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -645,10 +645,12 @@ function confCloudJS() {
     function addIconNextToVersion(newMsg, flagID, expDays) {
       $('.versionIcon').attr('data-original-title', newMsg).attr('title', newMsg);
       $('.versionIcon').on('mouseenter', function() {
-        $('.aui-tooltip').css('margin-top','100px')
-        setTimeout(function() { 
-          $('.aui-tooltip').css('z-index',125).css('margin-top','unset').removeClass('hidden').fadeIn(1000);
-        }, 500);
+        waitForElm('.aui-tooltip').then((elm) => {
+          $('.aui-tooltip').css('margin-top','100px');
+          setTimeout(function() { 
+            $('.aui-tooltip').css('z-index',125).css('margin-top','unset').removeClass('hidden').fadeIn(1000);
+          }, 500);
+        });
       });
       $('.versionIcon').on('mouseleave', function() {
         $('.aui-tooltip').css('z-index','unset').css('margin-top','100px').addClass('hidden').fadeOut(1000);

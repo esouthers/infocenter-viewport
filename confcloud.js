6e8c07f5a260aa25a31cf70ca031d7a664e6f1d9
esouthers <153128812+esouthers@users.noreply.github.com>
2024-01-30 14:58:25 -0600

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -98,7 +98,7 @@ function confCloudJS() {
 
 
         waitForElm('.search-results__results__label').then((elm) => {
-          updateSearchResults.observe(document.querySelector('[data-vp-id="search-page-results"]'), {attributes: false, childList: true, characterData: true, subtree:true});
+          updateSearchResults.observe(document.querySelector('[data-vp-id="search-page-results"]'), {attributes: false, childList: false, characterData: false, subtree:false});
           let searchTerm = $('.vp-search-input__input').val();
           let numResults = $(elm).text().split(' result')[0];
           let plural = '';

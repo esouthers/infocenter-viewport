92142c9d565612a7dac1fb1250b87c2274964d64
esouthers <153128812+esouthers@users.noreply.github.com>
2024-03-05 15:31:54 -0600

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -265,11 +265,11 @@ function confCloudJS() {
             $('.search-header').after('<p class="search-header-text">Showing results <span id="startIdx">' + searchIdx[0] + '</span> to <span id="stopIdx">' + searchIdx[1] + '</span>.</p>');
             $(elm).hide();
 
-/*            $('.vp-search-result').each(function() {
+            $('.vp-search-result').each(function() {
               let tempText = $('.vp-search-result__content-source', this).text();
               $('.vp-search-result__content-source', this).text(tempText + ' ' + $('.vp-search-result__labels .aui-lozenge', this).text());
               $('.vp-search-result__labels', this).remove();
-            }); */
+            });
           });
           $('.vp-pagination__inner button').on('click', function(e){
             waitForElm('.search-results__results__label').then((elm) => {

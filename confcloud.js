a44685717928f9babb2e238064c3155f6d9b9223
esouthers <153128812+esouthers@users.noreply.github.com>
2024-03-05 14:43:17 -0600

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -234,9 +234,16 @@ function confCloudJS() {
             }
             let searchedSpacePrefix = $('[data-vp-id="search-page-horizontal-filter-content"]~[form="search-form"][type="hidden"]').attr('value');
             let searchedVersion = $('[data-vp-id="search-page-horizontal-filter-versions"] .vp-dropdown__button-label').text();
-            $('.header__navigation--heading').text(searchedSpaceName + ' ' + searchedVersion).attr('href','/' + searchedSpacePrefix);
-            $('.vp-desktop-navigation__page-tree__tree .vp-tree-item__header__title').attr('href','/' + searchedSpacePrefix);
-            $('.breadcrumbs a[rel="prev"]').text(searchedSpaceName + ' ' + searchedVersion).attr('href','/' + searchedSpacePrefix);
+            if (searchedVersion != '') {
+              $('.header__navigation--heading').text(searchedSpaceName + ' ' + searchedVersion).attr('href','/' + searchedSpacePrefix + '/' + searchedVersion);
+              $('.vp-desktop-navigation__page-tree__tree .vp-tree-item__header__title').attr('href','/' + searchedSpacePrefix + '/' + searchedVersion);
+              $('.breadcrumbs a[rel="prev"]').text(searchedSpaceName + ' ' + searchedVersion).attr('href','/' + searchedSpacePrefix + '/' + searchedVersion);
+            }
+            else {
+              $('.header__navigation--heading').text(searchedSpaceName).attr('href','/' + searchedSpacePrefix);
+              $('.vp-desktop-navigation__page-tree__tree .vp-tree-item__header__title').attr('href','/' + searchedSpacePrefix);
+              $('.breadcrumbs a[rel="prev"]').text(searchedSpaceName).attr('href','/' + searchedSpacePrefix);
+            }
           });
           
           $('.vp-pagination__inner button').click(function() {

18842e0cfd2a17e424c7e8847271c9809ebf62c3
esouthers <153128812+esouthers@users.noreply.github.com>
2024-03-08 16:30:34 -0600

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -256,12 +256,12 @@ function confCloudJS() {
                 if ($('.vp-search-result__labels .aui-lozenge', this).length > 1) {
                   let spacePlusVersion = $('.vp-search-result__labels .aui-lozenge', this).last().text() + ' ' + $('.vp-search-result__labels .aui-lozenge', this).first().text();
                   $('.vp-search-result__content-source', this).text(spacePlusVersion);
-                  $(this).append('<div class="texttemp" sylte="display:none;">' + spacePlusVersion + '</div>');
+                  $(this).append('<div class="texttemp" style="display:none;">' + spacePlusVersion + '</div>');
                 }
                 else {
                   let spacePlusVersion = tempText + ' ' + $('.vp-search-result__labels .aui-lozenge', this).text();
                   $('.vp-search-result__content-source', this).text(spacePlusVersion);
-                  $(this).append('<div class="texttemp" sylte="display:none;">' + spacePlusVersion + '</div>');
+                  $(this).append('<div class="texttemp" style="display:none;">' + spacePlusVersion + '</div>');
                 }
                 $('.vp-search-result__labels', this).remove();
                 $('.vp-search-result__content-source', this).addClass('done');

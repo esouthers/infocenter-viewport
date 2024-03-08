51713b26a21d4dcdc7fd38eceeec5895a1bd3f11
esouthers <153128812+esouthers@users.noreply.github.com>
2024-03-08 16:52:57 -0600

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -260,18 +260,18 @@ function confCloudJS() {
                 }
                 $('.vp-search-result__labels', this).remove();
                 $('.vp-search-result__content-source', this).addClass('done');
+                var fixSearchLabels = new MutationObserver(function(mutations) {
+                  fixSearchLabels.disconnect();
+                  $('.vp-search-result').each(function() {
+                    if ($('.vp-search-result__content-source', this).text() != $('.vp-search-result__content-source .texttemp', this).text()) {
+                      $('.vp-search-result__content-source', this).text($('.vp-search-result__content-source .texttemp', this).text());
+                    }
+                    fixSearchLabels.observe(document.querySelector('.vp-search-result__content-source.done', this), {childList: true, characterData: false, subtree:true});
+                  });
+                });
+                fixSearchLabels.observe(document.querySelector('.vp-search-result__content-source.done', this), {childList: true, characterData: false, subtree:true});
               }
             });
-            var fixSearchLabels = new MutationObserver(function(mutations) {
-              fixSearchLabels.disconnect();
-              $('.vp-search-result').each(function() {
-                if ($('.vp-search-result__content-source', this).text() != $('.vp-search-result__content-source .texttemp', this).text()) {
-                  $('.vp-search-result__content-source', this).text($('.vp-search-result__content-source .texttemp', this).text());
-                }
-              });
-              fixSearchLabels.observe(document.querySelector('.vp-search-result__content-source.done'), {childList: true, characterData: false, subtree:true});
-            });
-            fixSearchLabels.observe(document.querySelector('.vp-search-result__content-source.done'), {childList: true, characterData: false, subtree:true});
           }
           function pollVisibility() {
             if (!$('.vp-search-page__loading').is(":visible")) {

a67b68af84fe0ebc8f4b1089f9884cbe4ccea1cf
esouthers <153128812+esouthers@users.noreply.github.com>
2024-03-11 13:37:47 -0500

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -57,7 +57,9 @@ function confCloudJS() {
       let sidebar = '<div class="vp-article__aside-left no-print"><div id="vp-js-desktop__navigation" class="vp-article__aside-left__inner"><nav id="3ry00fx860k" aria-label="Main" class="vp-desktop-navigation__page-tree vp-scrollable-container"><div class="vp-tree vp-desktop-navigation__page-tree__tree"><ul class="vp-tree__container relative m-0 outline-none" role="tree"></ul></div></nav></div></div>';
       $('#content').before(sidebar);
       if (page404) {
-        process404pages();
+        if (window.location.hostname.indexOf('beta') > 0) {
+          process404pages();
+        }
       }
       function process404pages() {
         let pathname = window.location.pathname;

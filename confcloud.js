c7dc947e64c9ce2edbde37aba5e25b61b4e1e72b
esouthers <153128812+esouthers@users.noreply.github.com>
2024-03-12 10:27:12 -0500

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -245,7 +245,9 @@ function confCloudJS() {
               $('[data-vp-id="search-page-horizontal-filter"]').removeClass('hidden');
             }
           });
-          showSearchFilters.observe(document.querySelector('.vp-search-page__subgrid'), {childList: true, characterData: false, subtree:true});
+          if (window.location.host.indexOf('beta') < 0) {
+            showSearchFilters.observe(document.querySelector('.vp-search-page__subgrid'), {childList: true, characterData: false, subtree:true});
+          }
           function updateEachResultSpaceVersion() {
             $('.vp-search-result').each(function() {
               if (($('.vp-search-result__labels', this).length > 0) && !($('.vp-search-result__content-source', this).hasClass('done'))) {

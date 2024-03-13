a063588c23a611320162861c29d9d29514aae371
esouthers <153128812+esouthers@users.noreply.github.com>
2024-03-13 15:28:12 -0500

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -403,7 +403,7 @@ function confCloudJS() {
           if ($(window).width() > 640) {
             updateSidebar.disconnect();
             $('#vp-js-desktop__navigation').removeClass('vp-article__aside-left__inner--collapsed');
-//            updateSidebar.observe(document.querySelector('#vp-js-desktop__navigation'), {attributes: true, childList: false, characterData: false, subtree:false});
+            updateSidebar.observe(document.querySelector('#vp-js-desktop__navigation'), {attributes: true, childList: false, characterData: false, subtree:false});
           }
         }
       });

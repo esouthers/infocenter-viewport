89b4d8a90d9a7e5824667dd62ddab6cbafcf482a
esouthers <153128812+esouthers@users.noreply.github.com>
2024-03-13 14:55:18 -0500

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -34,6 +34,9 @@ function confCloudJS() {
     var productRedirectJSON = '{"Command360":[{"redirect":"Command360"}],"CDVR":[{"redirect":"CDVR"}],"CS":[{"redirect":"CoolSign"}],"EMS":[{"redirect":"EMS"}],"Furnace":[{"redirect":"Furnace"}],"hai1000":[{"redirect":"hai1000"}],"HaiHelper":[{"redirect":"Helper"}],"HMG":[{"redirect":"HMG"}],"HMP":[{"redirect":"HMP"}],"Hub":[{"redirect":"Hub"}],"Hub360":[{"redirect":"Hub360"}],"KB":[{"redirect":"KB"}],"Kraken":[{"redirect":"Kraken"}],"LightFlow":[{"redirect":"LightFlow"}],"MakitoDec":[{"redirect":"MakitoDec"}],"MakitoEnc":[{"redirect":"MakitoEnc"}],"MakitoXDec":[{"redirect":"MakitoXDec"}],"MakitoXEnc":[{"redirect":"MakitoXEnc"}],"MFXE":[{"redirect":"MFXE"}],"MakitoX1Enc":[{"redirect":"MakitoX1Enc"}],"MakitoX4Dec":[{"redirect":"MakitoX4Dec"}],"MakitoX4Enc":[{"redirect":"MakitoX4Enc"}],"Mantaray":[{"redirect":"Mantaray"}],"MJPiOS":[{"redirect":"MJPiOS"}],"HPM":[{"redirect":"HPM"}],"HPPM":[{"redirect":"HPPM"}],"PlayProiOS":[{"redirect":"PlayProiOS"}],"STB":[{"redirect":"Play1000STB"}],"Play20004000STB":[{"redirect":"Play20004000STB"}],"Stingray":[{"redirect":"Stingray"}],"StreamHub":[{"redirect":"StreamHub"}],"Torpedo":[{"redirect":"Torpedo"}]}';
     var viewportList = parseViewportData();    
     var isBetaSite = window.location.host.indexOf('beta') >= 0 ? true : false;
+    if (isBetaSite) {
+      $('body').addClass('beta');
+    }
 
 //    $('#vp-js-desktop__navigation').removeClass('vp-article__aside-left__inner--collapsed');
     function parseViewportData() {
@@ -193,9 +196,6 @@ function confCloudJS() {
         updateSidebar();
 
         processSearchPage();
-        if (isBetaSite) {
-          $('body').addClass('beta');
-        }
         function processSearchPage() { 
           let searchedSpaceKey = $('#search-form [name="s"]').attr('value');
           let searchedVersion = $('#search-form [name="v"]').attr('value') !== undefined ? $('#search-form [name="v"]').attr('value') : "";

b6c64a6c19f94b1831dba6f65b198e2e0df35ad4
esouthers <153128812+esouthers@users.noreply.github.com>
2024-04-05 14:09:41 -0500

    Update confcloud.js

--- confcloud.js
+++ confcloud.js
@@ -44,7 +44,7 @@ scrollHelpCenter.collection.members = scrollHelpCenter.collection.members.sort( 
     var productRedirectJSON = '{"Command360":[{"redirect":"Command360"}],"CDVR":[{"redirect":"CDVR"}],"CS":[{"redirect":"CoolSign"}],"EMS":[{"redirect":"EMS"}],"Furnace":[{"redirect":"Furnace"}],"hai1000":[{"redirect":"hai1000"}],"HaiHelper":[{"redirect":"Helper"}],"HMG":[{"redirect":"HMG"}],"HMP":[{"redirect":"HMP"}],"Hub":[{"redirect":"Hub"}],"Hub360":[{"redirect":"Hub360"}],"KB":[{"redirect":"KB"}],"Kraken":[{"redirect":"Kraken"}],"LightFlow":[{"redirect":"LightFlow"}],"MakitoDec":[{"redirect":"MakitoDec"}],"MakitoEnc":[{"redirect":"MakitoEnc"}],"MakitoXDec":[{"redirect":"MakitoXDec"}],"MakitoXEnc":[{"redirect":"MakitoXEnc"}],"MFXE":[{"redirect":"MFXE"}],"MakitoX1Enc":[{"redirect":"MakitoX1Enc"}],"MakitoX4Dec":[{"redirect":"MakitoX4Dec"}],"MakitoX4Enc":[{"redirect":"MakitoX4Enc"}],"Mantaray":[{"redirect":"Mantaray"}],"MJPiOS":[{"redirect":"MJPiOS"}],"HPM":[{"redirect":"HPM"}],"HPPM":[{"redirect":"HPPM"}],"PlayProiOS":[{"redirect":"PlayProiOS"}],"STB":[{"redirect":"Play1000STB"}],"Play20004000STB":[{"redirect":"Play20004000STB"}],"Stingray":[{"redirect":"Stingray"}],"StreamHub":[{"redirect":"StreamHub"}],"Torpedo":[{"redirect":"Torpedo"}]}';
     var viewportList = parseViewportData();
     var hiddenSpaces = ['Inclusion Library'];
-    var transmittersVariants = [{'variant' : 'Air', 'versions' : ['5.3.1']}, {'variant' : 'Pro3', 'versions' : ['5.3.1']}, {'variant' : 'Rack200 & Rack300', 'versions' : ['5.3.1']}, {'variant' : 'Pro460', 'versions' : ['3.5']}, {'variant' : 'Rack400', 'versions' : ['3.5']}];
+    var transmittersVariants = [{'variant' : 'Air', 'variantId' : 'b7d408e64aa8cb3c490819e3c48a8325', 'versions' : ['5.3.1']}, {'variant' : 'Pro3', 'variantId' : '86da7b3392964e34996b43f4446a4589', 'versions' : ['5.3.1']}, {'variant' : 'Rack200 & Rack300', 'variantId' : 'ffb7362a6e50642fa62d414e7c4ad118', 'versions' : ['5.3.1']}, {'variant' : 'Pro460', 'variantId' : 'a13d712ff5175366a5251e0bddae761a', 'versions' : ['3.5']}, {'variant' : 'Rack400', 'variantId' : '61d254219f7ed6aa202a26272bef005a', 'versions' : ['3.5']}];
     var pdfDialog = '<section role="dialog" id="pdf-dialog" data-aui-modal="true" class="hai-dialog aui-dialog2 card" aria-hidden="true" style="display:none"><header class="aui-dialog2-header card-header">' + svgPDF + '<h2 class="haiui-heading-03-book">Export to PDF</h2><a class="card-close"><span class="aui-icon aui-icon-small hai-icon-download-close dialog-cancel-button">Close</span></a></header><div class="card-body haiui-body-02 options"><label for="paperSize">Paper Size:</label><input type="radio" name="paperSize" id="letter" value="letter"><label for="letter">Letter</label><input type="radio" name="paperSize" id="a4" value="a4"><label for="a4">A4</label><div class="status-error" style="display: none;"><div class="aui-icon aui-icon-large hai-icon-status-error"></div><div class="error-text">Please select a size.</div></div></div><div class="card-body haiui-body-02 status" style="display:none;"><div id="progress-bar" class="aui-progress-indicator" data-value="0"><span class="aui-progress-indicator-value" style="width: 0px;"></span></div><ul class="status-message"><li id="PDFstep1" class="pdfStepWaiting"><span class="aui-icon aui-icon-small"></span>Queuing Export</li><li id="PDFstep2" class="pdfStepPending"><span class="aui-icon aui-icon-small"></span>Collecting Pages</li><li id="PDFstep3" class="pdfStepPending"><span class="aui-icon aui-icon-small"></span>Processing Pages <span class="numPDFDone"></span></li><li id="PDFstep4" class="pdfStepPending"><span class="aui-icon aui-icon-small"></span>Rendering PDF <span class="numPDFDone"></span></li></ul><div class="status-done" style="display:none;"><div class="aui-icon aui-icon-large hai-icon-status-ok"></div><div class="done-text">Your PDF has been created and will start downloading in a few seconds! If the download doesn\'t start, <a id="PDFDonelink">click here</a>.</div></div><div class="status-error" style="display:none;"><div class="aui-icon aui-icon-large hai-icon-status-error"></div><div class="error-text">Error. Something went wrong. Please <a href="mailto:infodev@haivision.com">contact us</a> regarding this issue.</div></div></div><footer class="card-footer"><div class=""><button class="primary dialog-start-button">Start</button><button class="primary dialog-cancel-button" style="display:none;">Cancel</button><button class="primary dialog-close-button" style="display:none;">Close</button></div></footer></section>';
     var pdfTemplateIDLetter = '8a044238-48b2-4484-9ec6-fdbc3e7df4f5';
     var pdfTemplateIDA4    = 'b0be2f1b-fac2-43b6-9256-673607eac403';
@@ -1984,13 +1984,21 @@ scrollHelpCenter.collection.members = scrollHelpCenter.collection.members.sort( 
           let idLetter = overrideTemplateLetter == '' ? pdfTemplateIDLetter : overrideTemplateLetter;
           let idA4 = overrideTemplateA4 == '' ? pdfTemplateIDA4 : overrideTemplateA4;
           var pdfTemplateID = $('input[name="paperSize"]:checked').val() == 'a4' ? idA4 : idLetter;
-          var variantToShow = viewportList.currentContentSource.variants == undefined ? '' : viewportList.currentContentSource.variants.current.name;
+          var variantToShow = '';
+          if (viewportList.currentContentSource.variants != undefined) {
+            $.each(transmittersVariants, function(i,j) {
+              if (j.variant == viewportList.currentContentSource.variants.current.name) {
+                variantToShow = j.variantId;
+                return false;
+              }
+            });
+          }
           var apiData = JSON.stringify({
             'pageId': $('body').attr('pageid'),
             'scope': 'descendants',
             'templateId': pdfTemplateID,
             'locale': 'en-US',
-            'variantId': 'a13d712ff5175366a5251e0bddae761a',
+            'variantId': variantToShow,
             'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone
           });
           $.ajax({
